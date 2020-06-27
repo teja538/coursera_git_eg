470ea0d5ef00d4adac6ade276991a520bc633e2b
teja <saisree538@gmail.com>
2020-06-27 18:33:47 +0530

    Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,13 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit , Input } from '@angular/core';
+import { Dish } from '../shared/dish';
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.css']
+})
+
+
+export class DishdetailComponent implements OnInit {
+
+@Input() dish:Dish; //same variable as in menu comp selector(component interaction from parent to child)
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
