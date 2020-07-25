b2394f151a97b8fa4ca33ebcb49befa720f6722d
teja <saisree538@gmail.com>
2020-07-25 18:17:28 +0530

    Promises part-2(added spinner to dish servive)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+//ang mat spinner
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -68,6 +70,7 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
 
     FormsModule,
     ReactiveFormsModule
